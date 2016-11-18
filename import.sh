b1df6bc84e7110bda19e3aab396c66a9fbc05f07
alexmsierra <alexm.sierra@gmail.com>
2016-11-18 13:55:28 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -16,6 +16,11 @@ cp $i cred_cred.csv
 mysqlimport -v --local --ignore-lines=1 --fields-terminated-by=, --columns='title,username,password,description' --local -u root -p rattic cred_cred.csv
 
 #set required parameters
+mysql -v -u root rattic -e \
+"UPDATE cred_cred set created=NOW() where created='0000-00-00 00:00:00';
+UPDATE cred_cred set modified=NOW() where created='0000-00-00 00:00:00';
+UPDATE cred_cred set iconname=Key.png where iconname=NULL;
+UPDATE cred_cred set descriptionmarkdow=0 where descriptionmarkdow=NULL;"
 
 #Cleanup
 rm cred_cred.csv
