6eceb943c6a3369aa1d38126141b5533488df037
alexmsierra <alexm.sierra@gmail.com>
2016-11-18 13:38:30 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-#copy csv file
+display_usage() { 
+	echo "Please provide the csv file name" 
+	echo -e "\nUsage:\n$0 file.csv \n" 
+}
+
+if [ "$#" -ne 1 ]; then
+   display_usage
+fi
+
+#copy csv file for the correct table name
+cp $i cred_cred.csv
 
 #import to DB
+mysqlimport -v --local --ignore-lines=1 --fields-terminated-by=, --columns='title,username,password,description' --local -u root -p rattic cred_cred.csv
 
-#set parameters
+#set required parameters
+
+#Cleanup
+rm cred_cred.csv
