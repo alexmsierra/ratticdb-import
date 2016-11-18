80f70bc73d859e49fcdc4694d6f897c1fd880814
alexmsierra <alexm.sierra@gmail.com>
2016-11-18 15:02:56 +0100

    Update import.sh
    SQL update bug fix

--- import.sh
+++ import.sh
@@ -20,7 +20,7 @@ mysqlimport -v --local --ignore-lines=1 --fields-terminated-by=, --columns='titl
 mysql -v -u root rattic -e \
 "UPDATE cred_cred set created=NOW() where created='0000-00-00 00:00:00';
 UPDATE cred_cred set modified=NOW() where modified='0000-00-00 00:00:00';
-UPDATE cred_cred set iconname='Key.png' where iconname=NULL;
+UPDATE cred_cred set iconname='Key.png' where iconname='';
 COMMIT;"
 
 #Housekeeping
